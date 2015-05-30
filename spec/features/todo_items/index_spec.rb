21c16f3ef11b8b0d19ae33be8c015cc03986abb1
Seth Kroger <seth.kroger@gmail.com>
2015-05-30 16:09:24 -0700

    Added helper modules to specs

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -5,13 +5,6 @@ end
 
 describe "Viewing todo items" do
   let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list.") }
-  
-  def visit_todo_list(list)
-    visit "/todo_lists"
-    within "#todo_list_#{list.id}" do
-      click_link "List Items"
-    end
-  end
 
   it "displays the title of the todo list" do
     visit_todo_list(todo_list)
