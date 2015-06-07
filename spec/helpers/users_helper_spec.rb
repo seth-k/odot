f533dfb8c755fdaa36d0f3ac817237394c073c0d
Seth Kroger <seth.kroger@gmail.com>
2015-06-07 14:57:18 -0700

    Validate user for unique email

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,15 +1,7 @@
 require 'spec_helper'
+RSpec.configure do |c|
+  c.expose_current_running_example_as :example
+end
 
-# Specs in this file have access to a helper object that includes
-# the UsersHelper. For example:
-#
-# describe UsersHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe UsersHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,54 @@
 require 'spec_helper'
+RSpec.configure do |c|
+  c.expose_current_running_example_as :example
+end
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) {
+    {
+      first_name: "Seth",
+      last_name: "Kroger",
+      email: "foo@example.com"
+    }
+  }
+  context "validations" do
+    let(:user) { User.new(valid_attributes) }
+    
+    before do
+      User.create(valid_attributes)
+    end
+    
+    it "requires an email do" do
+      expect(user).to validate_presence_of(:email)
+    end
+    
+    it "requires a unique email" do
+      expect(user).to validate_uniqueness_of(:email)
+    end
+    
+    it "requires a unique email case-insensitive" do
+      user.email = "JASON@TEAMTREEHOUSE.COM"
+      expect(user).to validate_uniqueness_of(:email)
+    end
+    
+  end
+  
+  context "#downcase_email" do
+    it "makes the email attribute lower case" do
+      user = User.new(valid_attributes.merge(email: "JASON@TEAMTREEHOUSE.COM"))
+      #user.downcase_email
+      #expect(user.email).to eq("jason@teamtreehouse.com")
+      expect{ user.downcase_email }.to change{ user.email }
+        .from("JASON@TEAMTREEHOUSE.COM")
+        .to("jason@teamtreehouse.com")
+    end
+    
+    it "downcases an email before saving" do
+      user = User.new(valid_attributes)
+      user.email = "MIKE@TEAMTREEHOUSE.COM"
+      expect(user.save).to be true
+      expect(user.email).to eq("mike@teamtreehouse.com")
+    end
+  end
+  
 end
